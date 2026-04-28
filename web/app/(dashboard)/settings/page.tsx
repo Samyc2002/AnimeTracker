65ea54a44dc1473566487e6325b4d4df4aa9198e
Samriddha Chattopadhyay <200020040@iitdh.ac.in>
2026-04-29 03:37:19 +0530

    Remove database seed utility after one-time use

--- web/app/(dashboard)/settings/page.tsx
+++ web/app/(dashboard)/settings/page.tsx
@@ -7,7 +7,6 @@ import { Query, ID } from 'appwrite';
 import { account, databases, DATABASE_ID, PROFILES_COLLECTION_ID, WATCHLIST_COLLECTION_ID, WATCHED_EPISODES_COLLECTION_ID } from '@/lib/appwrite';
 import { fetchUserList, mediaToWatchlistEntry } from '@/lib/anime-provider';
 import RequireAuth from '@/components/RequireAuth';
-import DatabaseSeed from '@/components/DatabaseSeed';
 import { enqueueSnackbar } from 'notistack';
 
 interface ProfileDoc {
@@ -416,7 +415,6 @@ function SettingsPage() {
           </div>
         </div>
 
-        <DatabaseSeed />
       </div>
     </div>
   );
